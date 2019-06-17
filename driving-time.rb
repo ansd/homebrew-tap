35fd4636a98b10ec432f4882fbc1f8f1b787811b
David Ansari <david.ansari@sap.com>
2019-06-17 13:25:38 +0200

    Fix os

--- driving-time.rb
+++ driving-time.rb
@@ -5,18 +5,20 @@ class DrivingTime < Formula
   if OS.mac?
     url "https://github.com/ansd/driving-time/releases/download/v#{version}/driving-time-#{version}-darwin-amd64"
     sha256 'cb7f5401a540cfa75a9161c446aedd42c42d4ac45893f333f5499ed1afeda334'
-    os 'darwin'
   elsif OS.linux?
     url "https://github.com/ansd/driving-time/releases/download/v#{version}/driving-time-#{version}-linux-amd64"
     sha256 'c3590a85b2a3a61c8597480e5eb680e32e00368d4743549e63e4a58d23ade45d'
-    os 'linux'
   end
 
   depends_on :arch => :x86_64
 
   def install
     binary_name = 'driving-time'
-    bin.install "driving-time-#{version}-#{os}-amd64" => binary_name
+    if OS.mac?
+      bin.install "driving-time-#{version}-darwin-amd64" => binary_name
+    elsif OS.linux?
+      bin.install "driving-time-#{version}-linux-amd64" => binary_name
+    end
   end
 
   test do
